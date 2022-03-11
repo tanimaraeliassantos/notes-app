09cd3391090d121434e406a3130549c1b5561505
Tanimara Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2022-03-11 14:10:57 +1100

    Update data.js

--- src/data.js
+++ src/data.js
@@ -2,18 +2,18 @@ export const data = [
   {
     id: 1,
     body: `### This just in!
-        
-        Markdown is an *awesome* way to write **very basic HTML** by typing manually
-        
-        It can be used for:
-        
-        1. README files
-        1.Slack and Discord messages
-        1. **So much more!**
-        
-        ---
-        
-        Here's a division :point_up:`,
+
+Markdown is an *awesome* way to write **very basic HTML** by typing manually
+
+It can be used for:
+
+1. README files
+1. Slack and Discord messages
+1. **So much more!**
+
+---
+
+Here's a division 👆`,
   },
   { id: 2, body: `a` },
   { id: 3, body: `b` },
